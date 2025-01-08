40a806007ddf8cf8d807e807d1d52e4975dc87ca
Khiew Jian Bin <khiewbin@gmail.com>
2025-01-08 12:01:42 +0800

    add hierachy highligh editor script

---
+++ Assets/EditorTools/Helpers/UnityEditorHelpers.cs
@@ -0,0 +1,6 @@
+﻿using UnityEditor;
+
+    public static class UnityEditorHelpers
+    {
+        public static bool IsHierarchyWindowFocused => EditorWindow.focusedWindow != null && EditorWindow.focusedWindow.titleContent.text == "Hierarchy";
+    }
